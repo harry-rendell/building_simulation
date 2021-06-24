508deb1f8d1c2a394db03fd6655a3ddcf72337fb
Harry Rendell <harryrendell9@gmail.com>
2021-06-24 20:08:24 +0100

    Fitting model to real data

--- funcs/fitting.py
+++ funcs/fitting.py
@@ -1,14 +1,12 @@
 import numpy as np
 
-def loss(params, method, times, Ts_true):
+def loss(params, method, times, Ts_true, T_out, Q_in):
 	"""
 	Loss function to pass to optimiser. Note that this only currently works with a 2 segment set-up.
-
 	Parameters
 	----------
 	params : list of floats
 			list of [k1, k2, c1, c2, T1, T2]
-
 	method : str
 		choose a method to calculate loss.
 		Possible choices:
@@ -21,8 +19,7 @@ def loss(params, method, times, Ts_true):
 	error : float
 			loss using method given
 	"""
-
-	T_out = 10
+	
 	k1, k2, c1, c2, T1, T2 = params
 
 	k_est = np.array([k1,k2]).reshape(1,-1)
@@ -32,12 +29,12 @@ def loss(params, method, times, Ts_true):
 	T_out = np.array(T_out).reshape(-1,1)
 	Ts = np.empty(shape=(len(times), 1, 2))
 	Ts[0] = T_est
-	Q_in = 1000
 
 	dtimes = np.diff(times)
 	for i in range(len(times)-1):
 
 		heat_flux = np.diff(np.hstack((T_est,T_out)))*k_est
+# 		heat_flux = np.diff(np.hstack((T_est,T_out[i])))*k_est
 		net_heat_flux = np.diff(heat_flux)
 
 		# update non central nodes
@@ -45,6 +42,7 @@ def loss(params, method, times, Ts_true):
 
 		# update central node
 		T_est[:, 0] += ( (heat_flux[:,0] + Q_in) / c_est[:,0] ).sum() * dtimes[i]
+# 		T_est[:, 0] += ( (heat_flux[:,0] + Q_in[i]) / c_est[:,0] ).sum() * dtimes[i]
 
 		Ts[i+1] = T_est
 
@@ -66,3 +64,67 @@ def loss(params, method, times, Ts_true):
 		raise ValueError('Method not supported')
 
 	return error
+
+def loss_single_seg(params, method, times, Ts_true, T_out, Q_in):
+	"""
+	Loss function to pass to optimiser. Note that this only currently works with a 2 segment set-up.
+	Parameters
+	----------
+	params : list of floats
+			list of [k, c, T]
+	method : str
+		choose a method to calculate loss.
+		Possible choices:
+			mse - mean square error
+			mad - mean absolute deviation
+			lae - least absolute error
+			sse - sum of squares
+	Returns
+	-------
+	error : float
+			loss using method given
+	"""
+
+# 	if len(params) == 3:
+# 		k, c, T, = params
+# 		k_est = np.array(k).reshape(1,-1)
+# 		c_est = np.array(c).reshape(1,-1)
+# 		T_est = np.array(T).reshape(1,-1)
+# 	elif len(params) == 6:
+# 		k1, k2, c1, c2, T1, T2 = params
+# 		k_est = np.array([k1,k2]).reshape(1,-1)
+# 		c_est = np.array([c1,c2]).reshape(1,-1)
+# 		T_est = np.array([T1,T2]).reshape(1,-1)
+# 	else:
+# 		print('Parameter shape mismatch')
+	
+# 	T_out = 10
+# 	Q_in = 1000
+	k, c, T_est = params
+	
+	Ts = np.empty(shape=(len(times)))
+	Ts[0] = T_est
+				  
+	dtimes = np.diff(times)
+	for i in range(len(times)-1):
+		T_est += ( Q_in[i] + k*(T_out[i] - T_est) )/c * dtimes[i]
+		Ts[i+1] = T_est
+
+# 	Ts = np.transpose(Ts,axes=(1,2,0)) # New shape has (n_branches, n_segments, n_iterations)
+
+	if method=='mse':
+		# mean square error
+		error = np.mean((Ts_true - Ts) ** 2)
+	elif method=='mad':
+		# mean absolute deviation
+		error = np.mean(abs(Ts_true-Ts))
+	elif method=='lae':
+		# least absolute error
+		error = np.sum(abs(Ts_true-Ts))
+	elif method=='sse':
+		# sum of squares error
+		error = np.sum((Ts_true - Ts) ** 2)
+	else:
+		raise ValueError('Method not supported')
+
+	return error, Ts
--- funcs/models.py
+++ funcs/models.py
@@ -2,143 +2,143 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class model_ND():
-    def __init__(self, name, k, c, T, T_out, Q_in):
-        """
-        Initialise class and set system parameters
+	def __init__(self, name, k, c, T, T_out, Q_in):
+		"""
+		Initialise class and set system parameters
 
-        Parameters
-        ---------
-        name : str
-            Name of model, used in axis legend
-        k : array of floats
-            Thermal conductivity of segments
-        c : array of floats
-            Heat capacity of segments
-        T : array of floats
-            Initial temperature of segments
-        T_out : float
-            Outside temperature (Celcius)
-        Q_in : float
-             Heat supply to segment 0 (Watts)
-        """
-        self.name  = name
-        self.k     = np.array(k) # Thermal conductivity of segments
-        self.c     = np.array(c) # Heat capacity of segments
-        self.T     = np.array(T) # Temperatures of segments
-        self.T_out = np.array(T_out).reshape(-1,1)
-        self.T_out = self.T_out
-        self.Q_in_mag  = Q_in
-        
-        # These conditional statements are a little convoluted, find a better way of setting n_br, n_seg
-        if len(self.T.shape) == 0:
-            n_seg = 1
-            n_br  = 1
-        elif len(self.T.shape) == 1:
-            n_seg = self.T.shape[0]
-            n_br  = 1
-        else:
-            n_br, n_seg = self.T.shape
-            
-        # If only given parameters for a single segment (number), or for a single branch (vector), recast arrays
-        if len(self.T.shape) < 2:
-            shape      = (1,-1) # Calculate the appropriate shape to cast to
-            self.k     = self.k.reshape(shape)
-            self.c     = self.c.reshape(shape)
-            self.T     = self.T.reshape(shape)
-        
-        self.n_br  = n_br # Number of branches
-        self.n_seg = n_seg # Number of segments per branch 
-        # NOTE currently all branches must have same no. of segments, need to change this (or have empty segments to ensure same shape)
-        
-        assert (len(self.k) == n_br) & (len(self.c) == n_br), 'input parameter shape mismatch'
-        
-    def update(self, dt):
-        """
-        Calculate heat flux between segments and update temperatures
+		Parameters
+		---------
+		name : str
+			Name of model, used in axis legend
+		k : array of floats
+			Thermal conductivity of segments
+		c : array of floats
+			Heat capacity of segments
+		T : array of floats
+			Initial temperature of segments
+		T_out : float
+			Outside temperature (Celcius)
+		Q_in : float
+			 Heat supply to segment 0 (Watts)
+		"""
+		self.name  = name
+		self.k	 = np.array(k) # Thermal conductivity of segments
+		self.c	 = np.array(c) # Heat capacity of segments
+		self.T	 = np.array(T) # Temperatures of segments
+		self.T_out = np.array(T_out).reshape(-1,1)
+		self.T_out = self.T_out
+		self.Q_in_mag  = Q_in
 
-        Parameters
-        ----------
-        dt : float
-            Time interval to update new temperatures
-        """
-        heat_flux = np.diff(np.hstack((self.T,self.T_out)))*self.k
-        net_heat_flux = np.diff(heat_flux)
-        
-        # update non central nodes
-        self.T[:, 1:] += ( net_heat_flux / self.c[:,1:] ) * dt
-        
-        # update central node
-        self.T[:, 0] += ( (heat_flux[:,0] + self.Q_in) / self.c[:,0] ).sum() * dt
-        
-    def run(self, times, func_Q_in):
-        """
-        Run simulation for given number of iterations with given time values
+		# These conditional statements are a little convoluted, find a better way of setting n_br, n_seg
+		if len(self.T.shape) == 0:
+			n_seg = 1
+			n_br  = 1
+		elif len(self.T.shape) == 1:
+			n_seg = self.T.shape[0]
+			n_br  = 1
+		else:
+			n_br, n_seg = self.T.shape
 
-        Parameters
-        ----------
-        times : array
-            set of time values to run the simulation
-        func_Q_in : function
-            Function of time which modulates heat input
-            
-        Returns
-        -------
-        Ts : ndarray, shape (number of iterations, number of segments)
-            numpy array of temperatures for each segment, for each time step
-        """
-        Ts = np.empty(shape=(len(times), self.n_br, self.n_seg))
-        Ts[0] = self.T
-        Qs = [self.Q_in_mag * func_Q_in(0)]
-        self.times = times
-        dtimes = np.diff(times)
-        for i, t in enumerate(times[1:]):
-            self.Q_in = self.Q_in_mag * func_Q_in(t) # allow input data to be used
-            self.update(dtimes[i])
-            Ts[i+1] = self.T
-            Qs.append(self.Q_in)
-            
-        Ts = np.transpose(Ts,axes=(1,2,0)) # New shape has (n_branches, n_segments, n_iterations)
-        self.Ts = Ts
-        self.Qs = Qs
-            
-        return Ts, Qs
-    
-    def plot(self, figax=None, show_heating=True, **kwargs):
-        """
-        Plot the simulation
+		# If only given parameters for a single segment (number), or for a single branch (vector), recast arrays
+		if len(self.T.shape) < 2:
+			shape	  = (1,-1) # Calculate the appropriate shape to cast to
+			self.k	 = self.k.reshape(shape)
+			self.c	 = self.c.reshape(shape)
+			self.T	 = self.T.reshape(shape)
 
-        Parameters
-        ----------
-        figax : tuple
-            tuple of (figure, axis handle). If None, generate new fig, ax
-        show_heating : bool
-            True to show the heating profile on the same plot
-        kwargs
-            extra keyword arguments to pass to ax.plot
-            
-        Returns
-        -------
-        fig : figure
-        ax  : axis handle
-        """
-        
-        # Create new figure and axes handles if none supplied
-        if figax == None:
-            fig, ax = plt.subplots(1,1, figsize=(18,6))
-        else:
-            fig, ax = figax
-        
-        for i, T_br in enumerate(self.Ts):
-            for j, T_seg in enumerate(T_br[1:]):
-                ax.plot(self.times/3600, T_seg, label=self.name+': branch: {}, segment: {}'.format(i,j), color='rbgcm'[i+j], **kwargs)
-        ax.plot(self.times/3600, self.Ts[0,0,:], label=self.name+': Central temperature', color='k', **kwargs)
-        ax.set(xlabel='Time (hours)', ylabel='Temperature (C)')
-        ax.legend()
-        
-        if show_heating:
-            ax2 = ax.twinx()
-            ax2.plot(self.times/3600, self.Qs, ls='--', lw=2, color='r', label='Heat input')
-            ax2.set(ylabel='Heat input (W)')
-            ax2.legend(loc=7)
-            
-        return fig, ax
+		self.n_br  = n_br # Number of branches
+		self.n_seg = n_seg # Number of segments per branch
+		# NOTE currently all branches must have same no. of segments, need to change this (or have empty segments to ensure same shape)
+
+		assert (len(self.k) == n_br) & (len(self.c) == n_br), 'input parameter shape mismatch'
+
+	def update(self, dt):
+		"""
+		Calculate heat flux between segments and update temperatures
+
+		Parameters
+		----------
+		dt : float
+			Time interval to update new temperatures
+		"""
+		heat_flux = np.diff(np.hstack((self.T,self.T_out)))*self.k
+		net_heat_flux = np.diff(heat_flux)
+
+		# update non central nodes
+		self.T[:, 1:] += ( net_heat_flux / self.c[:,1:] ) * dt
+
+		# update central node
+		self.T[:, 0] += ( (heat_flux[:,0] + self.Q_in) / self.c[:,0] ).sum() * dt
+
+	def run(self, times, func_Q_in):
+		"""
+		Run simulation for given number of iterations with given time values
+
+		Parameters
+		----------
+		times : array
+			set of time values to run the simulation
+		func_Q_in : function
+			Function of time which modulates heat input
+
+		Returns
+		-------
+		Ts : ndarray, shape (number of iterations, number of segments)
+			numpy array of temperatures for each segment, for each time step
+		"""
+		Ts = np.empty(shape=(len(times), self.n_br, self.n_seg))
+		Ts[0] = self.T
+		Qs = [self.Q_in_mag * func_Q_in(0)]
+		self.times = times
+		dtimes = np.diff(times)
+		for i, t in enumerate(times[1:]):
+			self.Q_in = self.Q_in_mag * func_Q_in(t) # allow input data to be used
+			self.update(dtimes[i])
+			Ts[i+1] = self.T
+			Qs.append(self.Q_in)
+
+		Ts = np.transpose(Ts,axes=(1,2,0)) # New shape has (n_branches, n_segments, n_iterations)
+		self.Ts = Ts
+		self.Qs = Qs
+
+		return Ts, Qs
+
+	def plot(self, figax=None, show_heating=True, **kwargs):
+		"""
+		Plot the simulation
+
+		Parameters
+		----------
+		figax : tuple
+			tuple of (figure, axis handle). If None, generate new fig, ax
+		show_heating : bool
+			True to show the heating profile on the same plot
+		kwargs
+			extra keyword arguments to pass to ax.plot
+
+		Returns
+		-------
+		fig : figure
+		ax  : axis handle
+		"""
+
+		# Create new figure and axes handles if none supplied
+		if figax == None:
+			fig, ax = plt.subplots(1,1, figsize=(18,6))
+		else:
+			fig, ax = figax
+
+		for i, T_br in enumerate(self.Ts):
+			for j, T_seg in enumerate(T_br[1:]):
+				ax.plot(self.times/3600, T_seg, label=self.name+': branch: {}, segment: {}'.format(i,j), color='rbgcm'[i+j], **kwargs)
+		ax.plot(self.times/3600, self.Ts[0,0,:], label=self.name+': Central temperature', color='k', **kwargs)
+		ax.set(xlabel='Time (hours)', ylabel='Temperature (C)')
+		ax.legend()
+
+		if show_heating:
+			ax2 = ax.twinx()
+			ax2.plot(self.times/3600, self.Qs, ls='--', lw=2, color='r', label='Heat input')
+			ax2.set(ylabel='Heat input (W)')
+			ax2.legend(loc=7)
+
+		return fig, ax
