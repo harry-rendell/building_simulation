79a3bed068774692c3cb8423f2f18bfa178c2b9f
Harry Rendell <harryrendell9@gmail.com>
2021-07-07 18:27:29 +0100

    new loss function that keeps temperature constant

--- funcs/fitting.py
+++ funcs/fitting.py
@@ -67,7 +67,7 @@ def loss(params, method, times, Ts_true, T_out, Q_in):
 
 def loss_single_seg(params, method, times, Ts_true, T_out, Q_in):
 	"""
-	Loss function to pass to optimiser. Note that this only currently works with a 2 segment set-up.
+	Loss function to pass to optimiser. Note that this only currently works with a 1 segment set-up.
 	Parameters
 	----------
 	params : list of floats
@@ -114,13 +114,62 @@ def loss_single_seg(params, method, times, Ts_true, T_out, Q_in):
 
 	return error
 
+def loss_single_seg_fixed_T(params, method, times, T0, Ts_true, T_out, Q_in):
+	"""
+	Loss function to pass to optimiser. Note that this only currently works with a 1 segment set-up.
+	Parameters
+	----------
+	params : list of floats
+			list of [k, c]
+	method : str
+		choose a method to calculate loss.
+		Possible choices:
+			mse - mean square error
+			mad - mean absolute deviation
+			lae - least absolute error
+			sse - sum of squares
+	Returns
+	-------
+	error : float
+			loss using method given
+	"""
+	k, c = params
+	
+	Ts = np.empty(shape=(len(times)))
+	Ts[0] = T0
+				  
+	dtimes = np.diff(times)
+	
+	for i in range(len(times)-1):
+		dT_est = ( Q_in[i] + k*(T_out[i] - Ts[i]) )/c * dtimes[i]
+		Ts[i+1] = Ts[i] + dT_est
+
+# 	Ts = np.transpose(Ts,axes=(1,2,0)) # New shape has (n_branches, n_segments, n_iterations)
+
+	if method=='mse':
+		# mean square error
+		error = np.mean((Ts_true - Ts) ** 2)
+	elif method=='mad':
+		# mean absolute deviation
+		error = np.mean(abs(Ts_true-Ts))
+	elif method=='lae':
+		# least absolute error
+		error = np.sum(abs(Ts_true-Ts))
+	elif method=='sse':
+		# sum of squares error
+		error = np.sum((Ts_true - Ts) ** 2)
+	else:
+		raise ValueError('Method not supported')
+
+	return error
+
 def loss_two_seg(params, method, times, T0, Ts_true, T_out, Q_in):
 	"""
 	Loss function to pass to optimiser. Note that this only currently works with a 2 segment set-up.
 	Parameters
 	----------
 	params : list of floats
-			list of [k, c, T]
+			list of [k, c]
 	method : str
 		choose a method to calculate loss.
 		Possible choices:
